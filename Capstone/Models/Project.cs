4be86ff33f0d1618c17c196174438aa4e75585ac
Kalen Rose <kalenrose@outlook.com>
2018-09-18 20:55:12 -0400

    Add models for User, Task, Project, and PersonalNote

---
+++ Capstone/Models/Project.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Project
+    {
+        private int projectId;
+        private string title, description, scopeStatement;
+        private DateTime startDate, dueDate, completionDate;
+
+        public void LoadProject(){
+            //TODO
+        }
+
+        /*
+        public Document[] LoadDocuments(){
+
+        }
+        */
+
+        public void GetNextMilestone(){
+            //TODO
+        }
+
+        public void NextMilestoneCompletion(){
+            //TODO
+        }
+
+        public void TasksCompletion(){
+            //TODO
+        }
+
+        public void WorkCompletion(){
+            //TODO
+        }
+    }
+}
---
+++ Capstone/Models/Task.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Capstone.Models
+{
+    public class Task
+    {
+        private int taskId, workLoad, priority;
+        private string title, description;
+        private DateTime dueDate, startDate, completedDate;
+    }
+}
