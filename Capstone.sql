c488441bb3d0dfcc7113182ffdfb6d1d0fbc3b5e
Jatin Rampal <jatinrampal@hotmail.com>
2018-09-30 17:55:57 -0400

    Blob Storage

--- Capstone.sql
+++ Capstone.sql
@@ -3,3 +3,5 @@ FROM Task
 JOIN Project P on Task.projectId = P.projectId
 JOIN User_Task Task2 on Task.taskId = Task2.taskId
 WHERE Task2.uName = 'Kish' AND P.projectId = '1';
+
+SELECT * FROM [User]
