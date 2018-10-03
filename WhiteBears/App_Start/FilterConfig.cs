24d5236262971d04cc48993726505f6a26f79b08
Jatin Rampal <jatinrampal@hotmail.com>
2018-10-03 15:06:26 -0400

    Revert "Revert "Dashboard""

--- WhiteBears/App_Start/FilterConfig.cs
+++ WhiteBears/App_Start/FilterConfig.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:Capstone/App_Start/FilterConfig.cs
 ﻿using System.Web;
 using System.Web.Mvc;
 
@@ -11,3 +12,18 @@ namespace WhiteBears
         }
     }
 }
+=======
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WhiteBears
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
+>>>>>>> master:WhiteBears/App_Start/FilterConfig.cs
--- WhiteBears/App_Start/RouteConfig.cs
+++ WhiteBears/App_Start/RouteConfig.cs
@@ -1,3 +1,29 @@
+<<<<<<< HEAD:Capstone/App_Start/RouteConfig.cs
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WhiteBears
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+
+        }
+    }
+}
+=======
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,3 +47,4 @@ namespace WhiteBears
         }
     }
 }
+>>>>>>> master:WhiteBears/App_Start/RouteConfig.cs
