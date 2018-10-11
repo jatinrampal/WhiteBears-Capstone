b34e1ebc1bc7d19208815c2e2dbdf11a6b40a87c
Kalen Rose <kalenrose@outlook.com>
2018-10-11 18:11:02 -0400

    Add UserSettings view, controller, model.  View contains two tables, one to create user, another to modify or delete existing users.

---
+++ WhiteBears/Controllers/AdminPageController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WhiteBears.Models;
+
+namespace WhiteBears.Controllers {
+    public class AdminPageController : Controller {
+        // GET: AdminPage
+        public ActionResult Index() {
+            return View();
+        }
+
+        public ActionResult UserSettings() {
+            if(Session["username"] != null) {
+
+                List<User> users = new List<User>();
+
+                users.Add(new User("Kalen", "Rose", "Kalen", "Admin4", "PM"));
+                users.Add(new User("Jatin", "Rampal", "Jatin", "Admin4", "Wopm"));
+                users.Add(new User("Kish", "Dalal", "Kish", "Admin2", "Asas"));
+                users.Add(new User("Rafael", "Sigwalt", "Raf", "Admin1", "Esdasd"));
+                users.Add(new User("Mark", "Orlando", "Mark", "Admin100", "Teacher"));
+
+                //if (model.CurrentUser.Role == "Admin") {
+                    return View(users);
+                //} else {
+                //    return RedirectToAction("Index", "Dashboard");
+                //}
+            } else {
+                return RedirectToAction("Index", "Home");
+            }
+        }
+    }
+}
---
+++ WhiteBears/Models/AddUserModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WhiteBears.Models {
+    public class AddUserModel {
+        public User CurrentUser { get; set; }
+    }
+}
