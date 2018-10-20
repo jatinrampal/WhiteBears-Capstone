e36c091449890a1bcbaf302dfb9a2ebcfb2fda86
Jatin Rampal <jatinrampal@hotmail.com>
2018-10-20 13:44:18 -0400

    Blob implementation

--- Capstone.sql
+++
@@ -1,5 +0,0 @@
-SELECT Task.title, Task.description, Task.startDate, Task.dueDate, Task.completionDate
-FROM Task
-JOIN Project P on Task.projectId = P.projectId
-JOIN User_Task Task2 on Task.taskId = Task2.taskId
-WHERE Task2.uName = 'Kish' AND P.projectId = '1';
---
+++ Whitebears-BlobStorage/Controllers/BlobController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Whitebears_BlobStorage.Repository;
+using System.Threading.Tasks;
+
+namespace Whitebears_BlobStorage.Controllers
+{
+    public class BlobController : Controller
+    {
+        private readonly IBlobStorageRepository repo;
+        public BlobController(IBlobStorageRepository _repo)
+        {
+            this.repo = _repo;
+
+        }
+        // GET: Blob
+        public ActionResult Index()
+        {
+            var blobVM = repo.GetBlobs();
+            return View(blobVM);
+        }
+
+        public JsonResult RemoveBlob(string file, string extension)
+        {
+            bool isDeleted = repo.DeleteBlob(file, extension);
+            return Json(isDeleted, JsonRequestBehavior.AllowGet);
+        }
+
+        public async Task<ActionResult> DownloadBlob(string file, string extension)
+        {
+            bool isDownloaded = await repo.DownloadBlobAsync(file, extension);
+            return RedirectToAction("Index");
+
+        }
+        [HttpGet]
+        public ActionResult UploadBlob()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult UploadBlob(HttpPostedFileBase uploadFileName)
+        {
+            bool isUploaded = repo.UploadBlob(uploadFileName);
+            if(isUploaded == true)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+    }
+
+    
+}
