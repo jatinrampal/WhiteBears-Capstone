bfbdaf7115143efa0d31cbf10dcd893e322c78a9
Kalen Rose <kalenrose@outlook.com>
2018-11-09 15:55:26 -0500

    Fix autofocus issue

--- WhiteBears/Models/DashboardModel.cs
+++ WhiteBears/Models/DashboardModel.cs
@@ -6,18 +6,15 @@ using System.Linq;
 using System.Web;
 using WhiteBears;
 
-namespace WhiteBears.Models
-{
-    public class DashboardModel
-    {
+namespace WhiteBears.Models {
+    public class DashboardModel {
         public Project[] Projects { get; set; }
         public User CurrentUser { get; set; }
 
         [DisplayFormat(DataFormatString = "{MM-dd-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime CurrDate { get; set; }
 
-        public DataRow[] GetUser(String username)
-        {
+        public DataRow[] GetUser(String username) {
             DatabaseHelper dh = new DatabaseHelper();
             return dh.RunQuery($"SELECT * FROM [User] WHERE uName = '{username}';");
         }
@@ -30,16 +27,15 @@ namespace WhiteBears.Models
             return dr[0]["title"].ToString();
         }
 
-        public DataRow[] GetProjects(string username)
-        {
+        public DataRow[] GetProjects(string username) {
             DatabaseHelper dh = new DatabaseHelper();
+
             return dh.RunQuery($"SELECT * FROM Project p " +
                 $"INNER JOIN User_Project up ON p.projectId = up.projectId " +
                 $"WHERE up.uName = '{username}';");
         }
 
-        public Project GetProject(string username, string projectId)
-        {
+        public Project GetProject(string username, string projectId) {
             DatabaseHelper dh = new DatabaseHelper();
             DataRow[] drs = dh.RunQuery($"SELECT * FROM Project p " +
                 $"INNER JOIN User_Project up ON p.projectId = up.projectId " +
@@ -51,12 +47,11 @@ namespace WhiteBears.Models
             Task[] tasks = new Task[drs1.Count()];
 
             int i = 0;
-            foreach(DataRow dr1 in drs1){
+            foreach (DataRow dr1 in drs1) {
                 DateTime dueDate = Convert.ToDateTime(dr1["dueDate"]);
                 DateTime completionDate = Convert.ToDateTime(dr1["completionDate"]);
 
-                tasks[i++] = new Task
-                {
+                tasks[i++] = new Task {
                     Title = dr1["title"].ToString(),
                     Priority = dr1["priority"].ToString(),
                     DueDate = dueDate,
@@ -66,17 +61,16 @@ namespace WhiteBears.Models
                 };
             }
 
-            return new Project
-            {
+            return new Project {
                 ProjectId = Int32.Parse(drs[0]["projectId"].ToString()),
                 Title = drs[0]["title"].ToString(),
                 Tasks = tasks
             };
         }
 
-        public DataRow[] GetTasks(string sUser, int projectId)
-        {
+        public DataRow[] GetTasks(string sUser, int projectId) {
             DatabaseHelper dh = new DatabaseHelper();
+
             return dh.RunQuery($"SELECT * FROM Task t " +
                                $"JOIN Project p on t.projectId = p.projectId " +
                                $"JOIN User_Task ut on t.taskId = ut.taskId " +
@@ -84,29 +78,41 @@ namespace WhiteBears.Models
                                $"AND p.projectId='{projectId}'");
         }
 
-        public DataRow[] GetPersonalNote(string uName)
-        {
+        public List<User> GetTaskUsers(int taskId) {
+            DatabaseHelper dh = new DatabaseHelper();
+
+            DataRow[] drs = dh.RunQuery($"SELECT u.FirstName, u.LastName FROM [User] u " +
+                                "JOIN User_Task ut ON u.uName = ut.uName " +
+                                $"WHERE ut.taskId = '{taskId}'");
+
+            List<User> users = new List<User>();
+
+            foreach(DataRow dr in drs) {
+                users.Add(new User(dr["firstName"].ToString(), dr["lastName"].ToString()));
+            }
+
+            return users;
+        }
+
+        public DataRow[] GetPersonalNote(string uName) {
             DatabaseHelper dh = new DatabaseHelper();
             return dh.RunQuery($"SELECT * FROM PersonalNote WHERE PersonalNote.uName = '{uName}';");
         }
 
-        public DataRow[] AddPersonalNote(string uName, string note)
-        {
+        public DataRow[] AddPersonalNote(string uName, string note) {
             DatabaseHelper dh = new DatabaseHelper();
             dh.RunUpdateQuery($"INSERT INTO PersonalNote VALUES('{uName}', '{note}', '{DateTime.Now}');");
 
             return dh.RunQuery($"SELECT MAX(noteId) FROM PersonalNote");
         }
 
-        public int DeletePersonalNote(int noteId)
-        {
+        public int DeletePersonalNote(int noteId) {
             DatabaseHelper dh = new DatabaseHelper();
             //DELETE FROM PersonalNote WHERE PersonalNote.uName = 'Kish' AND PersonalNote.noteId = 2;
             return dh.RunUpdateQuery($"DELETE FROM PersonalNote WHERE PersonalNote.noteId = '{noteId}';");
         }
 
-        public int UpdatePersonalNote(string note, int noteId)
-        {
+        public int UpdatePersonalNote(string note, int noteId) {
             DatabaseHelper dh = new DatabaseHelper();
             return dh.RunUpdateQuery($"UPDATE PersonalNote SET note='{note}' WHERE PersonalNote.noteId = '{noteId}';");
         }
