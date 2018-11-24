70b00b0e9c8f99489a62b4e6a3bc8092270fd804
Kalen Rose <kalenrose@outlook.com>
2018-11-23 22:35:25 -0500

    Sign up back-end

--- WhiteBears/Controllers/SignUpController.cs
+++ WhiteBears/Controllers/SignUpController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,26 +8,36 @@ using WhiteBears.Models;
 
 namespace WhiteBears.Controllers {
     public class SignUpController : Controller {
-        // GET: SignUp
         public ActionResult Index() {
             return View();
         }
 
         [HttpPost]
         public ActionResult CheckUsername(string username) {
-            //Database code goes here
-
-            return null;
+            DatabaseHelper dh = new DatabaseHelper();
+            if (dh.RunQuery($"SELECT * FROM [User] WHERE uName = '{username}'").Count() == 1) {
+                return Json(new { success = false });
+            } else {
+                return Json(new { success = true });
+            }
         }
 
-        [HttpPost]
-        public ActionResult Go(string firstname, string lastname, string username, string email, string password, string companyname, DateTime companyformed, string title, string description, string scope, DateTime startDate, DateTime dueDate) {
-            User u = new User(firstname, lastname, username, email, password, "admin");
-            Project p = new Project(title, description, scope, startDate, dueDate);
+        public ActionResult Go(string firstname, string lastname, string username, string email, string password, string companyname, DateTime companyformed, string title, string description, string scope, string startDate, string dueDate) {
+            User u = new User(firstname, lastname, username, email, password, "Admin");
             Company c = new Company(companyname, companyformed);
 
-            //Database code goes here
-            return null;
+            DatabaseHelper dh = new DatabaseHelper();
+
+            dh.RunInsertQuery($"INSERT INTO Company VALUES('{c.Name}')");
+            dh.RunInsertQuery($"INSERT INTO Project VALUES('{title}', '{description}', '{scope}', '{startDate}', '{dueDate}', '01/01/0001')");
+
+            DataRow[] drs = dh.RunQuery($"SELECT MAX(companyId) FROM Company");
+
+            string companyId = drs[0][0].ToString();
+
+            dh.RunInsertQuery($"INSERT INTO [User] VALUES('{u.Username}', '{companyId}', '{u.Password}', '{u.FirstName}', '{u.LastName}', '{u.Role}', 'true')");
+
+            return Json(new { success = true });
         }
     }
 }
